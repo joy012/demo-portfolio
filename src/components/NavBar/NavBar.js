636920ee68d7417bd4b3ad3f89bc11634229c1ff
joy012 <joy.buet.012@gmail.com>
2020-11-02 13:14:28 +0600

    smoot slide added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,7 +14,7 @@ const NavBar = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="#">Home</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#projects" smooth duration={1000}>Projects</Nav.Link>
           <Nav.Link href="#about">About</Nav.Link>
           <Nav.Link href="#blog">Blog</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
